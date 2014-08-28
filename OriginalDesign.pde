7a0d8844411f210111c1030ef11528fd3a3ecde3
unknown <cakacimi@697-334-Bree.sf.k12.internal>
2014-08-28 10:34:30 -0700

    originalDesign

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,59 @@
+int z=10;
+int y=1;
+int xPosition=250;
+int yPosition=250;
+
 void setup()
 {
-
+  size(500,500);
+  background(0);
 }
 void draw()
 {
-  ellipse(50,50,50,50);
-}
+  frameRate(10);
+  background(0);
+  smooth();
 
+  //red circle//
+  fill(255-y,0,0,100);
+  ellipse(xPosition,yPosition,z,z);
+  
+  //orange circle//
+  fill(255-y,140,0,100);
+  ellipse(xPosition,yPosition,z-50,z-50);
+  
+  //yellow circle//
+  fill(255-y,255,0,100);
+  ellipse(xPosition,yPosition,z-100,z-100);
+  
+  //green circle//
+  fill(0,255-y,0,100);
+  ellipse(xPosition,yPosition,z-150,z-150);
+  
+  //blue circle//
+  fill(0,0,255-y,100);
+  ellipse(xPosition,yPosition,z-200,z-200);
+  
+  //purple circle//
+  fill(186,85,211-y,100);
+  ellipse(xPosition,yPosition,z-250,z-250);
+  
+  //indigo circle//
+  fill(138,43,226-y,100);
+  ellipse(xPosition,yPosition,z-300,z-300);
+  
+  
+  //change//
+  
+  z= z+10;
+  y=y+3;
+  if (z>490)
+  {
+    z=10;
+  }
+  if (y<50)
+  {
+    y=1;
+  }
+
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -12,19 +12,69 @@ import java.io.InputStream;
 import java.io.OutputStream; 
 import java.io.IOException; 
 
-public class OriginalDesign extends PApplet {
+public class originalDesign extends PApplet {
+
+int z=10;
+int y=1;
+int xPosition=250;
+int yPosition=250;
 
 public void setup()
 {
-
+  size(500,500);
+  background(0);
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
-}
+  frameRate(10);
+  background(0);
+  smooth();
 
+  //red circle//
+  fill(255-y,0,0,100);
+  ellipse(xPosition,yPosition,z,z);
+  
+  //orange circle//
+  fill(255-y,140,0,100);
+  ellipse(xPosition,yPosition,z-50,z-50);
+  
+  //yellow circle//
+  fill(255-y,255,0,100);
+  ellipse(xPosition,yPosition,z-100,z-100);
+  
+  //green circle//
+  fill(0,255-y,0,100);
+  ellipse(xPosition,yPosition,z-150,z-150);
+  
+  //blue circle//
+  fill(0,0,255-y,100);
+  ellipse(xPosition,yPosition,z-200,z-200);
+  
+  //purple circle//
+  fill(186,85,211-y,100);
+  ellipse(xPosition,yPosition,z-250,z-250);
+  
+  //indigo circle//
+  fill(138,43,226-y,100);
+  ellipse(xPosition,yPosition,z-300,z-300);
+  
+  
+  //change//
+  
+  z= z+10;
+  y=y+3;
+  if (z>490)
+  {
+    z=10;
+  }
+  if (y<50)
+  {
+    y=1;
+  }
+
+}
   static public void main(String[] passedArgs) {
-    String[] appletArgs = new String[] { "OriginalDesign" };
+    String[] appletArgs = new String[] { "originalDesign" };
     if (passedArgs != null) {
       PApplet.main(concat(appletArgs, passedArgs));
     } else {
